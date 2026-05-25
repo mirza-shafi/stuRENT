ad43528d89fa51a7128f2ab8ca3fc9b91bc11b83
mirza-shafi <mirza.md.shafi.uddin@gmail.com>
2026-05-25 14:51:33 +0600

    Configure media files serving in production with DEBUG=False

--- backend/sturent/urls.py
+++ backend/sturent/urls.py
@@ -8,6 +8,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     # Django admin
     path("admin/", admin.site.urls),
@@ -17,6 +20,9 @@ urlpatterns = [
     path("api/v1/", include("apps.rental.urls", namespace="rental")),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in both development and production
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
